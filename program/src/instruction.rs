9e184326ddfa04fb696dd00fddf7d82cbb6408bb
peakyblinder <peakyblinder@solrazr.com>
2021-06-25 13:17:57 +0530

    added tests for instruction.rs

--- program/src/instruction.rs
+++ program/src/instruction.rs
@@ -3,9 +3,11 @@ use solana_program::{
     // pubkey::Pubkey,
 };
 use std::convert::TryInto;
+use std::mem::size_of;
 
 use crate::error::TokenWhitelistError::InvalidInstruction;
 
+#[derive(Clone, Debug, PartialEq)]
 pub enum TokenWhitelistInstruction {
 
     /// Accounts expected by InitTokenWhitelist
@@ -75,6 +77,27 @@ impl TokenWhitelistInstruction {
         })
     }
 
+    /// Packs a [TokenWhitelistInstruction](enum.TokenWhitelistInstruction.html) into a byte buffer.
+    pub fn pack(&self) -> Vec<u8> {
+        let mut buf = Vec::with_capacity(size_of::<Self>());
+        match self {
+            &Self::InitTokenWhitelist { max_whitelist_size } => {
+                buf.push(0);
+                buf.extend_from_slice(&max_whitelist_size.to_le_bytes());
+            }
+            &Self::AddToWhitelist{} => {
+                buf.push(1);
+            }
+            &Self::RemoveFromWhitelist{} => {
+                buf.push(2);
+            }
+            &Self::SetAllocationToZero{} => {
+                buf.push(3);
+            }
+        };
+        buf
+    }
+
     // fn unpack_pubkey(input: &[u8]) -> Result<(Pubkey, &[u8]), ProgramError> {
     //     if input.len() >= 32 {
     //         let (key, rest) = input.split_at(32);
@@ -84,4 +107,53 @@ impl TokenWhitelistInstruction {
     //         Err(InvalidInstruction.into())
     //     }
     // }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_pack_init_whitelist() {
+        let whitelist_size: u64 = 100;
+        let check = TokenWhitelistInstruction::InitTokenWhitelist {
+            max_whitelist_size: whitelist_size,
+        };
+        let packed = check.pack();
+        let mut expect = vec![0];
+        expect.extend_from_slice(&whitelist_size.to_le_bytes());
+        assert_eq!(packed, expect);
+        let unpacked = TokenWhitelistInstruction::unpack(&expect).unwrap();
+        assert_eq!(unpacked, check);
+    }
+
+    #[test]
+    fn test_pack_add_to_whitelist() {
+        let check = TokenWhitelistInstruction::AddToWhitelist{};
+        let packed = check.pack();
+        let expect = vec![1];
+        assert_eq!(packed, expect);
+        let unpacked = TokenWhitelistInstruction::unpack(&expect).unwrap();
+        assert_eq!(unpacked, check);
+    }
+
+    #[test]
+    fn test_pack_remove_from_whitelist() {
+        let check = TokenWhitelistInstruction::RemoveFromWhitelist{};
+        let packed = check.pack();
+        let expect = vec![2];
+        assert_eq!(packed, expect);
+        let unpacked = TokenWhitelistInstruction::unpack(&expect).unwrap();
+        assert_eq!(unpacked, check);
+    }
+
+    #[test]
+    fn test_pack_set_allocation_zero() {
+        let check = TokenWhitelistInstruction::SetAllocationToZero{};
+        let packed = check.pack();
+        let expect = vec![3];
+        assert_eq!(packed, expect);
+        let unpacked = TokenWhitelistInstruction::unpack(&expect).unwrap();
+        assert_eq!(unpacked, check);
+    }
 }
